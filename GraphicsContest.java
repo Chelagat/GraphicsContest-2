2f2ccec3af8e397a93c2c087ec02fd5739d539f5
S <s>
2015-11-23 03:39:49 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -1,15 +1,95 @@
-/*
- * File: GraphicsContest.java
- * --------------------------
- */
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
-import acm.program.*;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import acm.graphics.GLine;
+import acm.graphics.GMath;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.program.Program;
 import acm.graphics.*;
+import acm.gui.DoubleField;
+import acm.io.IODialog;
 
-public class GraphicsContest extends GraphicsProgram {
+public class GraphicsContest extends Program {
+	private DoubleField firstGuess;
+	private JButton root;
+	private JButton clear;
+	private BlankClass canvas;
+	private int degree;
+	private JButton inverse;
+	private ArrayList<Double> coefficient;
+	public void init(){
+		clear = new JButton("Clear");
+		add(clear, NORTH);
+		root = new JButton("Root");
+		add(root,NORTH);
+		inverse = new JButton("Inverse");
+		add(inverse, NORTH);
+		add(new JLabel("First guess for Newton's Approximation"), NORTH);
+		firstGuess = new DoubleField();
+		add(firstGuess, NORTH);
+		firstGuess.addActionListener(this);
 
-	public void run() {
-		/* Empty */
+		addActionListeners();
+		canvas = new BlankClass();
+		add(canvas);
+		coefficient = new ArrayList<Double>();
+		IODialog dialog = getDialog();
+		//dialog.setExceptionOnError(false);
+		degree = dialog.readInt("Enter degree of polynomial:");
+		int count = degree;
+		for (int i = 0; i < degree; i++) {
+
+			String line = dialog.readLine("Enter coefficient "+count);
+			double x = Double.parseDouble(line);
+			coefficient.add(x);
+			--count;
+		}
+		double x = dialog.readInt("Enter constant: ");
+		coefficient.add(x);
+		drawGraph();
+
+
 	}
 
+	private void drawGraph(){
+
+		canvas.addEntry(coefficient);
+		canvas.update();
+
+
+	}
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==clear){
+			canvas.clear();
+			coefficient.clear();
+			IODialog dialog = getDialog();
+			degree = dialog.readInt("Enter degree of polynomial");
+			int count = degree;
+			for (int i = 0; i < degree; i++) {
+
+				String line = dialog.readLine("Enter coefficient "+count);
+				double x = Double.parseDouble(line);
+				coefficient.add(x);
+				--count;
+			}
+			double x = dialog.readInt("Enter constant: ");
+			coefficient.add(x);
+			drawGraph();
+
+
+
+		} else if(e.getSource()==root){
+			canvas.findRoots(coefficient, firstGuess.getValue());
+		} else if(e.getSource()==inverse){
+			canvas.drawInverse(coefficient);
+		}
+
+	}
 }
+
